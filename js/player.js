53b61f098ed4b69125a8b9400475d4f5d074f720
Peter Klipfel <peter@klipfel.me>
2014-01-23 18:45:34 -0700

    placeholder for playlist

--- js/player.js
+++ js/player.js
@@ -1,15 +1,29 @@
 var SWPlayer = {
-  state : {currentTrackId : 0, playing : false, trackList: []},
+  state : {currentTrackId : 0, playing : false, trackList: [], playlistUp : false},
 
   build : function(){
     SWPlayer.buildBoilerPlate()
     SWPlayer.bindFrames()
     SWPlayer.bindPlayerClickHandlers()
+    SWPlayer.bindPlayListHandlers(0)
     SWPlayer.setPlayerData(SWPlayer.state.trackList[SWPlayer.state.currentTrackId])
   },
   
   buildBoilerPlate : function(){
-    $('html').append('<div id="soundwebPlayer"><div id="soundwebPlayerArtwork"></div><div id="soundwebPlayerTrackName"></div><br><div id="soundwebPlayerArtist"></div><div id="soundwebPlayerControls"><span id="soundwebPlayerPrev">Prev</span> <span id="soundwebPlayerPlay">Play</span> <span id="soundwebPlayerNext">Next</span></div></div>')
+    var html = '<div id="soundwebPlayer">'+
+                  '<div id="soundwebPlayerArtwork"></div>'+
+                  '<div id="soundwebPlayerTrackName"></div>'+
+                  '<br>'+
+                  '<div id="soundwebPlayerArtist"></div>'+
+                  '<div id="soundwebPlayerControls">'+
+                    '<span id="soundwebPlayerPrev">Prev</span>'+
+                    '<span id="soundwebPlayerPlay">Play</span>'+
+                    '<span id="soundwebPlayerNext">Next</span>'+
+                  '</div>'+
+                  '<div id=soundwebPlayerShowPlaylist>Show/Hide Playlist</div>'+
+                  '<div id=soundwebPlayerPlaylist></div>'+
+                '</div>'
+    $('html').append(html)
     var player = $('#soundwebPlayer')
     player.css({'background-color': '#f3f3f3', 'height': '70px', 'position': 'fixed', 'bottom': '0', 'width': '100%'})
   },
@@ -34,6 +48,19 @@ var SWPlayer = {
       SWPlayer.state.currentTrackId = (SWPlayer.state.currentTrackId+1)%SWPlayer.state.trackList.length
       SC.Widget(SWPlayer.state.trackList[SWPlayer.state.currentTrackId]).play()
       SWPlayer.state.playing = true
+    })
+  },
+
+  bindPlayListHandlers : function() {
+    $('#soundwebPlayerShowPlaylist').css({float:'right', 'margin-top':'-30px'})
+    $('#soundwebPlayerShowPlaylist').click(function(){
+      if(SWPlayer.state.playlistUp){
+        $('#soundwebPlayer').animate({'height': '70px'})
+        SWPlayer.state.playlistUp = false
+      } else {
+        $('#soundwebPlayer').animate({'height': $('soundwebPlayerPlaylist').height()+100})
+        SWPlayer.state.playlistUp = true
+      }
     })
   },
 
